d6063ddc1dcd09a4c52cdd02b956bb32c2e99255
anudi-D <82524171+anudi-D@users.noreply.github.com>
2021-04-16 22:04:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import kivy
+from kivy.config import Config
+
+
+Config.set('graphics', 'resizable', True)
+
+
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
@@ -7,14 +14,16 @@ from kivy.core.text import LabelBase
 import webbrowser
 from kivy.uix.screenmanager import ScreenManager,Screen
 from kivy.clock import Clock
-from kivy.storage.jsonstore import JsonStore
+
+kivy.require("1.9.1")
+
+
 import requests
 import json
-import time
 import csv
 
 s = ""
-f = open("mood_detector.csv",)
+f = open("mood_detector.csv")
 reader = csv.reader(f)
 feeling = list(reader)
 print (feeling)
@@ -25,8 +34,10 @@ for elem in feeling:
 
 print(mood)
 
+link = '<a href = "https://www.who.int/publications/i/item/9789240003927?gclid=Cj0KCQiApsiBBhCKARIsAN["> here </a>'
 
-mydict = {}
+link_work = link.format
+
 
 with open('Bot_question.csv', mode='r') as inp:
     reader = csv.reader(inp)
@@ -79,8 +90,6 @@ class Login(Screen):
 
 class Chat(Screen):
 
-
-
     def on_start(self):
         if self.ids.start_send.text == "send":
             self.send_button()
@@ -93,7 +102,7 @@ class Chat(Screen):
 
     def send_button(self):
 
-        if self.ids.namelabel.text  == "good" or  self.ids.namelabel.text == "that's nice" or self.ids.namelabel.text == "cool" or self.ids.namelabel.text == "That's good" or self.ids.namelabel.text == "That's awesome!":
+        if self.ids.namelabel.text  == "good" or  self.ids.namelabel.text == "that's nice" or self.ids.namelabel.text == "cool" or self.ids.namelabel.text == "That's good" or self.ids.namelabel.text == "That's awesome!" or self.ids.namelabel.text == "that's awesome!" or self.ids.namelabel.text == "that's good":
             self.ids.namelabel.text = ""
             self.ids.showcolby.text ="Can you answer these questions for me to get to know a little bit about you?"
             self.focus()
@@ -128,7 +137,7 @@ class Chat(Screen):
             print(self.predicted_label)
 
             if self.predicted_label == "lonely" or self.predicted_label == 'anger' or self.predicted_label == 'sad' or self.predicted_label == 'depressed' or self.predicted_label == 'anxiety':
-                self.ids.showcolby.text = "There are some steps you could follow to get out of this state of your mind \n You can listen to some cheerful music \n Watch movies \n Go for a walk \n These options will help you stay out of it. Keep this conversation going I will help you feel happy."
+                self.ids.showcolby.text = "To get out of this state of your mind \n You can listen to some cheerful music \n Watch movies \n Go for a walk "
                 Clock.schedule_once(self.steps,8)
 
             elif self.predicted_label == "happy":
@@ -145,7 +154,13 @@ class Chat(Screen):
             self.ids.namelabel.text = ""
             Clock.schedule_once(self.hobby, 4)
 
+        elif self.ids.namelabel.text == "oh":
+            self.ids.showcolby.text = "hmmm! yeah"
+            self.ids.helplabel.text = "ask COLBY: 'Can you laugh?'"
 
+        elif self.ids.namelabel.text == "hmmm" or self.ids.namelabel.text == "Hmmm" or self.ids.namelabel.text == "hm" or self.ids.namelabel.text == "hmm":
+            self.ids.showcolby.text = "yeah"
+            self.ids.helplabel.text = "ask COLBY: 'Do you have any hobbies?'"
 
         elif self.ids.namelabel.text in dict_from_csv.keys():
             userin = self.ids.namelabel.text
@@ -157,12 +172,28 @@ class Chat(Screen):
         elif self.ids.namelabel.text in hobby.keys():
             userhob = self.ids.namelabel.text
             self.ids.showcolby.text = hobby[userhob]
-            if self.predicted_label == "lonely" or self.predicted_label == "anger" or self.predicted_label == 'sad' or self.predicted_label == 'depressed' or predicted_label == 'anxiety':
+            if self.predicted_label == "lonely" or self.predicted_label == "anger" or self.predicted_label == 'sad' or self.predicted_label == 'depressed' or self.predicted_label == 'anxiety':
                 Clock.schedule_once(self.pandeq, 4)
 
+            elif self.predicted_label == "happy":
+                self.ids.showcolby.text = "You can ask anything from me to make it more interesting!"
+                Clock.schedule_once(self.hmqu,6)
+
+        elif self.ids.namelabel.text == "Thankyou" or self.ids.namelabel.text == "thankyou" or self.ids.namelabel.text == "Thanks" or self.ids.namelabel.text == "thanks":
+            self.ids.showcolby.text = "I am always here for you!"
+
+        elif self.ids.namelabel.text == "yes" or self.ids.namelabel.text == "Yes" or self.ids.namelabel.text == "yeah" or self.ids.namelabel.text == "Yeah" or self.ids.namelabel.text == "I think so" or self.ids.namelabel.text == "yeah that's right" or self.ids.namelabel.text == "hmmm yes":
+            self.ids.namelabel.text = ""
+            self.ids.showcolby.text = "Don't worry this pandemic will be over soon in the future! Then you can be with your friends or relatives."
+            Clock.schedule_once(self.suggest,4)
+        elif self.ids.namelabel.text == "nope" or self.ids.namelabel.text == "not really" or self.ids.namelabel.text == "Nope, not really" or self.ids.namelabel.text == "no" or self.ids.namelabel.text == "Nope" or self.ids.namelabel.text == "No" or self.ids.namelabel.text == "Not really":
+            self.ids.namelabel.text = ""
+            self.ids.showcolby.text = "Ok that's good!"
 
         elif self.ids.namelabel.text == "wow" or self.ids.namelabel.text == "amazing" or self.ids.namelabel.text == "you're amazing":
+
             self.ids.showcolby.text = "yeah, thanks!"
+
         else:
             self.ids.showcolby.text = "I didn't get that, can you rephrase it?"
             Clock.schedule_once(self.user_Input,11)
@@ -184,7 +215,11 @@ class Chat(Screen):
         self.ids.showcolby.text = "How are you feeling right now?"
     def user_Input(self, *args):
         self.ids.helplabel.text = "ask COLBY:'Can you suggest any games to play?'"
-
+        Clock.schedule_once(self.suggest2,8)
+    def suggest(self, *args):
+        self.ids.namelabel.text = ""
+        self.ids.showcolby.text = "You can talk to trained counsellors confidentially, Take a call to thsese numbers! \n sumithriyo: 0112682535 \n CCC: 1333"
+        Clock.schedule_once(self.ask,4)
     def start(self, *args):
         self.ids.start_send.text = "send"
         self.ids.showcolby.text = ""
@@ -192,8 +227,18 @@ class Chat(Screen):
     def next(self, *args):
         self.ids.showcolby.text = ""
         self.ids.showcolby.text = "I am COLBY, I will speak with you and entertain you."
+    def ask(self, *args):
+        self.ids.showcolby.text = "You can ask anything from me to make it more interesting!"
+        self.ids.helplabel.text = "ask COLBY:'Can you suggest any games to play?'"
+
+
     def jhy(self, *args):
         self.ids.showcolby.text = "What is your ambition?"
+    def hmqu(self, *args):
+        self.ids.showcolby.text = "You can ask anything from me to make it more interesting!"
+
+    def suggest2(self,*args):
+        self.ids.helplabel.text = "Ask COLBY: 'Do you have any hobby?'"
 
     def helpbar(self, *args):
         self.ids.helplabel.text = "Try saying :- 'good'"
@@ -212,6 +257,7 @@ class Chat(Screen):
         self.ids.namelabel.text = ""
         self.ids.showcolby.text = ""
         self.ids.showcolby.text = f"Do you have any hobby? what is it {name}?"
+
     def focus(self, *args):
         self.ids.namelabel.focused = True
 
@@ -223,10 +269,6 @@ class Chat(Screen):
 class WindowManager(ScreenManager):
     pass
 
-
-
-
-Window.size = (400,650)
 
 LabelBase.register(name = 'Century Gothic',fn_regular= "Century Gothic 400.ttf")
 LabelBase.register(name = 'Myriad CAD',fn_regular= "myriadcad.otf")
@@ -242,14 +284,26 @@ LabelBase.register(name = 'Artifakt Element',fn_regular= "Artifakt Element Regul
 LabelBase.register(name = "MV Boli",fn_regular= "mvboli.ttf")
 
 
-KV = Builder.load_file('Fibz.kv')
+KV = Builder.load_file('COLBY_design.kv')
+
+Window.size = (400,650)
 
 class chatbot(App):
     def on_pause(self):
         return True
+    def password_check(self):
+        Chat.ids.user.text = "Username"
+        Chat.ids.user.color = (65 / 255, 125 / 255, 193 / 255, 1)
+        Chat.ids.passw.color = (65 / 255, 125 / 255, 193 / 255, 1)
+        Chat.ids.passw.text = "Password"
 
     def on_resume(self):
-        pass
+        if Chat.ids.namelabel.text != password and Chat.ids.namelabel.text != name:
+            self.password_check()
+
+        else:
+            pass
+
 
     def build(self):
         Window.clearcolor = (81/255,255/255,246/255,1)
